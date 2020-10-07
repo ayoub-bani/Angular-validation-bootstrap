ae2b8658569f351282c082cab2cf1aa38c952900
Ayoub Bani <ayoub.b@dsi-cs.com>
2020-10-07 11:46:19 +0000

    angular validation using bootstrap classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-login';
+  form = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+  });
+
+  get firstname() {
+    return this.form.get('firstName');
+  };
+  get lastname() {
+    return this.form.get('lastName');
+  }
 }
